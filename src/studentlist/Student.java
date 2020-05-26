c7c6513e0e9efb29e7bc4bfe72c6f7fe859a47dc
codeTEU <teu@sheridancollege.ca>
2020-05-26 19:08:11 -0400

    add address var, constructor, getter and setter

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -8,9 +8,22 @@ package studentlist;
 public class Student {
 
     private String name;
+    private String address;
+
+    public Student() {
+    }
 
     public Student(String name) {
         this.name = name;
+    }
+
+    public Student(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
     }
 
     public String getName() {
@@ -19,6 +32,15 @@ public class Student {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "name=" + name + ", address=" + address + '}';
     }
 
 }
